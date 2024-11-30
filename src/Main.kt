0a78be8e1cac62bcb1a571c709642f14c2f15995
himbeere <grossefeste@campus.tu-berlin.de>
2024-11-30 04:10:42 +0100

    Does not work, good enough for the evening

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,52 @@
 fun main() {
-    println("Hello World!")
+    var nodes = listOf(
+        Node(0, mapOf(Pair(0,0), Pair(1,2), Pair(2,2), Pair(3,3), Pair(4,300))),
+        Node(1, mapOf(Pair(0,12), Pair(1,0), Pair(2,2), Pair(3,3), Pair(4,300))),
+        Node(2, mapOf(Pair(0,4), Pair(1,2), Pair(2,0), Pair(3,3), Pair(4,300))),
+        Node(3, mapOf(Pair(0,2), Pair(1,2), Pair(2,2), Pair(3,0), Pair(4,300))),
+        Node(4, mapOf(Pair(0,4), Pair(1,2), Pair(2,2), Pair(3,3), Pair(4,0))),
+    )
+    val matrix = nodeToMatrix(nodes);
+    bellmanFord(matrix);
 }
 
-fun node_to_matrix(nodes: List<Node>) {
+
+/**
+ * Must follow
+ * 0...1...2...3
+ * 1...0...x...x
+ * 2...x...0...x
+ * 3...x...x...0
+ */
+
+fun nodeToMatrix(nodes: List<Node>) : Array<Array<Int>> {
     // note: Connections must always have the same order, so that node 0 is always followed by 1
-    var filteredNodes = nodes.stream().filter{ n : Node -> nodes.stream().filter{ n2: Node -> n2.identifier == n.identifier}.count().toInt() == 1}.toList();
+    var filteredNodes : ArrayList<Node> = ArrayList(nodes.stream().filter{ n : Node -> nodes.stream().filter{ n2: Node -> n2.identifier == n.identifier}.count().toInt() == 1}.toList());
     val matrix : Array<Array<Int>> = kotlin.Array(filteredNodes.size) { Array<Int>(nodes.size){0} }
-    for ((index, node) in matrix.withIndex()) {
-        for (i: Int in node.indices) {
-            node[i] = filteredNodes[index].connections[i] ?: Int.MAX_VALUE
+    for ((index, column) in matrix.withIndex()) {
+        for (row: Int in column.indices) {
+            column[row] = filteredNodes[index].connections[row] ?: Int.MAX_VALUE
         }
     }
+    return matrix;
 }
 
-fun bellman_ford(matrix: Array<Array<Int>>) {
-    for ((index, timeToNodeAtIndex) in matrix.withIndex()) {
-
+fun bellmanFord(matrix: Array<Array<Int>>) {
+    var changes = false;
+    for ((index, origin) in matrix.withIndex()) {
+        for (target:Int in origin.indices) {
+            for (altRoute:Int in matrix.indices.filterNot { f -> f == index}) {
+                if (target > altRoute + origin[altRoute]) {
+                    origin[target] = altRoute + origin[altRoute];
+                    changes = true;
+                }
+            }
+            print("$target  ");
+        }
+        println("")
     }
+    println("--------------")
+    if (changes) {
+        bellmanFord(matrix)
+    };
 }
--- src/Node.kt
+++ src/Node.kt
@@ -1,6 +1,6 @@
 import java.util.*
 import kotlin.collections.HashMap
 
-public class Node(public val identifier: Int, public val connections: HashMap<Int, Int>) {
+public class Node(public val identifier: Int, public var connections: Map<Int, Int>) {
 
 }
