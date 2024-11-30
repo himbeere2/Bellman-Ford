8acdf1fb5886687c50ed500b3c4eb4ecd5bed195
himbeere <grossefeste@campus.tu-berlin.de>
2024-11-30 04:12:48 +0100

    Does not work, good enough for the evening

--- src/Main.kt
+++ src/Main.kt
@@ -19,6 +19,7 @@ fun main() {
  * 3...x...x...0
  */
 
+// fixme: Matrix has same value throughout whole column
 fun nodeToMatrix(nodes: List<Node>) : Array<Array<Int>> {
     // note: Connections must always have the same order, so that node 0 is always followed by 1
     var filteredNodes : ArrayList<Node> = ArrayList(nodes.stream().filter{ n : Node -> nodes.stream().filter{ n2: Node -> n2.identifier == n.identifier}.count().toInt() == 1}.toList());
@@ -35,13 +36,13 @@ fun bellmanFord(matrix: Array<Array<Int>>) {
     var changes = false;
     for ((index, origin) in matrix.withIndex()) {
         for (target:Int in origin.indices) {
+            print("$target  ");
             for (altRoute:Int in matrix.indices.filterNot { f -> f == index}) {
                 if (target > altRoute + origin[altRoute]) {
                     origin[target] = altRoute + origin[altRoute];
                     changes = true;
                 }
             }
-            print("$target  ");
         }
         println("")
     }
